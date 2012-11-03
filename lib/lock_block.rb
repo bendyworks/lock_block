87418890be874ad031e775e5d2eac701edbcf1b6
Joe Nelson <cred+github@begriffs.com>
2012-11-03 14:40:39 -0500

    Reorder library

--- lib/lock_block.rb
+++ lib/lock_block.rb
@@ -3,17 +3,10 @@ require 'ripper'
 require 'digest/sha1'
 
 module LockBlock
-  def lock_tag source
-    tokens = Ripper.tokenize(source).select do |t|
-      t.gsub(/\s+/, "") != ''
-    end
-    Digest::SHA1.hexdigest tokens.to_s
-  end
-
   def decorate source
     indent = source.match(/^(\s+)/) ? $1 : ''
-    h = lock_tag source
-    "#{indent}# lock do #{h}\n#{source}#{indent}# lock end #{h}"
+    tag    = lock_tag source
+    "#{indent}# lock do #{tag}\n#{source}#{indent}# lock end #{tag}"
   end
 
   def resolve source
@@ -36,6 +29,13 @@ module LockBlock
       end
     end
     broken
+  end
+
+  def lock_tag source
+    tokens = Ripper.tokenize(source).select do |t|
+      t.gsub(/\s+/, "") != ''
+    end
+    Digest::SHA1.hexdigest tokens.to_s
   end
 
   def tags source
